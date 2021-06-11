442ca0bd4287d3fee9d06ae1419c276c17892fa1
Gabriel Reis Andrade <joaogabriel2@gmail.com>
2021-06-11 19:21:48 -0300

    Added enemy follow behavior and temporary Player.

---
+++ Assets/Scripts/EnemyFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollow : MonoBehaviour
+{
+    [SerializeField] float speed = 3f;
+    [SerializeField] float minimumDistanceFromPlayer = 1f;
+    private Transform playerTransform;
+    void Start()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, playerTransform.position) > minimumDistanceFromPlayer)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, playerTransform.position, speed * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/TempPlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempPlayerMovement : MonoBehaviour
+{
+    [SerializeField] float speed = 10.0f;
+    float horizontalInput;
+    float verticalInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector2.up * Time.deltaTime * speed * verticalInput);
+        transform.Translate(Vector2.right * Time.deltaTime *speed * horizontalInput);
+    }
+}
