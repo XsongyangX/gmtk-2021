dc212b7f807903038b106c247293e9fd3c55ba5d
Gabriel Reis Andrade <joaogabriel2@gmail.com>
2021-06-11 20:16:26 -0300

    Added basic enemy attack behavior.
    Enemies don't actually attack yet, only call the attack funcions.

---
+++ Assets/Scripts/EnemyBaseAttack.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBaseAttack : MonoBehaviour
+{
+    [SerializeField] private float minimumAttackDistance = 1.5f;
+    [SerializeField] private float cooldownTime = 2f;
+    float timeUntilNextAttack; 
+    private Transform playerTransform;
+    void Start()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if((Vector2.Distance(transform.position, playerTransform.position) <= minimumAttackDistance) && (timeUntilNextAttack < Time.time))
+        {
+            Attack();
+            Cooldown();
+        }
+    }
+    public virtual void Attack()
+    {
+        Debug.Log("Base attack");
+    }
+    public void Cooldown()
+    {
+        timeUntilNextAttack = Time.time + cooldownTime;
+    }
+}
---
+++ Assets/Scripts/RangedAttack.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangedAttack : EnemyBaseAttack
+{
+    public override void Attack()
+    {
+        Debug.Log("Ranged attack");
+    }
+}
