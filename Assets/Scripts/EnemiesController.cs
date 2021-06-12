4a0eaa13159e932f0bf0cb83cd27e3b64b406e3b
Gabriel Reis Andrade <joaogabriel2@gmail.com>
2021-06-12 16:04:00 -0300

    Added the enemies controller code and prefab.

---
+++ Assets/Scripts/EnemiesController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemiesController : MonoBehaviour
+{
+    [SerializeField] bool enemiesStartActive = false;
+    [SerializeField] GameObject enemiesHolder;
+    void Start()
+    {
+        AwakeEnemies(enemiesStartActive);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("hello there!");
+        if (other.CompareTag("Player"))
+        {
+            AwakeEnemies(true);
+        }
+    }
+
+    private void AwakeEnemies(bool activateEnemies)
+    {
+        enemiesHolder.SetActive(activateEnemies);
+    }
+}
