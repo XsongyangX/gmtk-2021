31f1fe47e0c6a9a2b55118929ff585950552c599
Carl Summers <31711543+CSumm@users.noreply.github.com>
2021-06-11 23:46:47 -0400

    Wrote script for Level hazard manager that deals with minor damage and instadeath

---
+++ Assets/Scripts/LevelHazard.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelHazard : MonoBehaviour
+{
+    PlayerHealth playerHealth;
+    // Start is called before the first frame update
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player" && this.tag == "BlackOoze")
+        {
+            playerHealth.loseHealth(-5f);
+        }
+        else if(collision.gameObject.tag == "Player" && this.tag == "InstaDeath")
+        {
+            playerHealth.loseHealth(-100f);
+        }
+        
+
+    }
+}
