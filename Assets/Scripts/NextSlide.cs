3115e730688ef45df7f27e3ecea82f415e2ec146
Carl Summers <31711543+CSumm@users.noreply.github.com>
2021-06-13 02:03:55 -0400

    Add tutorial UI and life counter

---
+++ Assets/Scripts/NextSlide.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class NextSlide : MonoBehaviour
+{
+    public string[] texts;
+    public Sprite[] sprites;
+    public Image demo;
+    public Text currText;
+    public Button nextButton;
+    int countUp = 0;
+
+    private void Update() {
+        if(countUp >= texts.Length)
+        {
+            nextButton.gameObject.SetActive(false);
+            return;
+        }
+
+        currText.text = texts[countUp];
+        demo.sprite = sprites[countUp];
+
+    }
+    
+    // Update is called once per frame
+
+    public void GoToNextSlide()
+    {
+        if (countUp < texts.Length)
+        {
+            countUp++;
+        }
+    }
+}
