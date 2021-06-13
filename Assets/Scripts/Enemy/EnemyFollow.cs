4b9e2d1a1927bb82c813af05daaf6fbfdff2fae0
Gabriel Reis Andrade <joaogabriel2@gmail.com>
2021-06-13 11:36:33 -0300

    Enemies point to the direction of movement.

--- Assets/Scripts/Enemy/EnemyFollow.cs
+++ Assets/Scripts/Enemy/EnemyFollow.cs
@@ -7,9 +7,13 @@ public class EnemyFollow : MonoBehaviour
     [SerializeField] float speed = 3f;
     [SerializeField] float minimumDistanceFromPlayer = 1f;
     private Transform playerTransform;
+    private Transform spriteTransform;
+    private float previousPositionX;
     void Start()
     {
         playerTransform = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        spriteTransform = gameObject.GetComponentInChildren<Transform>();
+
     }
 
     void Update()
@@ -22,5 +26,17 @@ public class EnemyFollow : MonoBehaviour
         {
             transform.position = Vector2.MoveTowards(transform.position, playerTransform.position, -speed * Time.deltaTime);
         }
+        float currentPositionX = transform.position.x;
+        Vector3 spriteScale = transform.localScale;
+        if(currentPositionX > previousPositionX+0.01)
+        {
+            spriteScale.x = -1;
+        }
+        else if(currentPositionX < previousPositionX-0.01)
+        {
+            spriteScale.x = 1;
+        }
+        transform.localScale = spriteScale;
+        previousPositionX = currentPositionX;
     }
 }
