9394f3d52fa1883aa67df88df3ecc24348381286
Gabriel Reis Andrade <joaogabriel2@gmail.com>
2021-06-12 21:31:22 -0300

    Added doors and the enemies on the tutorial.

---
+++ Assets/Scripts/DoorHolder.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorHolder : MonoBehaviour
+{
+    [SerializeField] GameObject[] doors;
+
+    public void OpenDoor(int doorNumber)
+    {
+        Debug.Log(doors.Length);
+        doors[doorNumber].SetActive(false);
+    }
+}
