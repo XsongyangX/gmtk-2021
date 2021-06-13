e0e43edec4cdcc7b0fcdc20dee13aeee300ec622
Carl Summers <31711543+CSumm@users.noreply.github.com>
2021-06-12 20:41:35 -0400

    Add base UI

---
+++ Assets/CanvasController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasController : MonoBehaviour
+{
+    public Canvas tutorialCanvas;
+
+   public void closeTutorialMenu()
+    {
+            Image img = this.GetComponentInParent(typeof(Image)) as Image;
+        if (img != null)
+            img.enabled = false;
+        }
+    }
