1a92a9dbf5e5b5cc4ae736bf597ec95de7ab132c
Carl Summers <31711543+CSumm@users.noreply.github.com>
2021-06-13 02:25:23 -0400

    Add health UI functionality

---
+++ Assets/HealthHUD.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthHUD : MonoBehaviour
+{
+    public Text livesLeft;
+    PlayerHealth playerHealth;
+
+    private void Start()
+    {
+        playerHealth = GameObject.Find("Player").GetComponent<PlayerHealth>();
+    }
+
+    private void Update()
+    {
+        livesLeft.text = playerHealth.Health.ToString();
+    }
+}
